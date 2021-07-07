1b5cbc2418df9d14663182e25d51ebc4ac7a633d
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-07 12:10:38 +0100

    move listing functions to accountHelper.ts, add info logging for different commands

--- accountHelper.ts
+++ accountHelper.ts
@@ -1,14 +1,17 @@
 import { getLogger } from "log4js";
 import { Account, Transaction } from "./models";
+import { formatter } from "./moneyHelper";
 
 const logger = getLogger('log');
 
-export function processTransactions(transactions: Transaction[], accounts: Map<string, Account>) : void {
+const accounts: Map<string, Account> = new Map();
+
+export function processTransactions(transactions: Transaction[]) : void {
     logger.info('Processing transactions');
-    transactions.forEach(transaction => processTransaction(transaction, accounts));
+    transactions.forEach(transaction => processTransaction(transaction));
 }
 
-function processTransaction(transaction: Transaction, accounts: Map<string, Account>) {
+function processTransaction(transaction: Transaction) {
     const senderKey = transaction.sender.toLowerCase();
     let senderAccount: Account = accounts.get(senderKey) || new Account(transaction.sender);
     if (!accounts.has(senderKey)) {
@@ -22,4 +25,42 @@ function processTransaction(transaction: Transaction, accounts: Map<string, Acco
         accounts.set(recKey, recAccount);
     }
     recAccount.balance += transaction.amountPence;
+}
+
+export function listAccounts(): void {
+    logger.info("Listing accounts");
+    accounts.forEach(account => {
+        console.log(account.name + '\t' +
+            formatter.format(account.balance / 100));
+    });
+}
+
+export function listTransactionsFromName(requestedName: string, transactions: Transaction[]) {
+    const requestedAccount = accounts.get(requestedName);
+    if (!requestedAccount) {
+        logger.error(`Account with requested name "${requestedName}" was not found.`);
+    }
+    else {
+        logger.info(`Listing transactions of account ${requestedName}`);
+        listTransactions(requestedAccount, transactions);
+    }
+}
+
+function listTransactions(account: Account, transactions: Transaction[]): void {
+    transactions.forEach(transaction => {
+        if (transaction.sender == account.name) {
+            console.log(transaction.date.toISODate() + '\t' +
+                'To\t' +
+                transaction.recipient + '\t' +
+                formatter.format(transaction.amountPence / 100) + '\t' +
+                transaction.narrative);
+        }
+        else if (transaction.recipient == account.name) {
+            console.log(transaction.date.toISODate() + '\t' +
+                'From\t' +
+                transaction.sender + '\t' +
+                formatter.format(transaction.amountPence / 100) + '\t' +
+                transaction.narrative);
+        }
+    })
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import { configure, getLogger } from "log4js";
-import { Account, Transaction } from "./models";
+import { Transaction } from "./models";
 import readline from 'readline';
 import { readTransactionsFromCsv } from "./csvHelper";
-import { processTransactions } from "./accountHelper";
+import { listAccounts, listTransactionsFromName, processTransactions } from "./accountHelper";
 import { readTransactionsFromJson } from "./jsonHelper";
-import { formatter } from "./moneyHelper";
 
 configure({
     appenders: {
@@ -18,33 +17,6 @@ configure({
 });
 const logger = getLogger('log');
 
-const accounts: Map<string, Account> = new Map();
-
-function listAccounts(): void {
-    accounts.forEach(account => {
-        console.log(account.name + '\t' +
-            formatter.format(account.balance / 100));
-    });
-}
-
-function listTransactions(account: Account): void {
-    transactions.forEach(transaction => {
-        if (transaction.sender == account.name) {
-            console.log(transaction.date.toISODate() + '\t' +
-                'To\t' +
-                transaction.recipient + '\t' +
-                formatter.format(transaction.amountPence / 100) + '\t' +
-                transaction.narrative);
-        }
-        else if (transaction.recipient == account.name) {
-            console.log(transaction.date.toISODate() + '\t' +
-                'From\t' +
-                transaction.sender + '\t' +
-                formatter.format(transaction.amountPence / 100) + '\t' +
-                transaction.narrative);
-        }
-    })
-}
 
 function processQuery(query: string) {
     if (query == 'list all') {
@@ -52,13 +24,7 @@ function processQuery(query: string) {
     }
     else if (query.startsWith("list ")) {
         const requestedName: string = query.slice(5);
-        const requestedAccount = accounts.get(requestedName);
-        if (!requestedAccount) {
-            logger.error(`Account with requested name "${requestedName}" was not found.`);
-        }
-        else {
-            listTransactions(requestedAccount);
-        }
+        listTransactionsFromName(requestedName, transactions);
     }
     else if (query == 'exit') {
         logger.info("Exiting program");
@@ -74,7 +40,7 @@ function processQuery(query: string) {
 /* user command */
 // const transactions: Transaction[] = readTransactionsFromCsv('data/DodgyTransactions2015.csv');
 const transactions: Transaction[] = readTransactionsFromJson('data/Transactions2013.json');
-processTransactions(transactions, accounts);
+processTransactions(transactions);
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
